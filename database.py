cafe421e21f8d094677875bc55b99915cdf25291
LeeShawnhun <5r2w132@gmail.com>
2024-09-12 17:22:34 +0900

    2024년 9월 12일 db 접속 정보 환경변수 처리

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:q1w2e3r4!!@ib-database.c9so6u826z2w.ap-northeast-2.rds.amazonaws.com:5432/postgres"
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
